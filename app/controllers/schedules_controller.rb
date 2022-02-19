fcee408954862041ffc3b09821caa9c18d41f6b0
sukeyuki <s.iwata1203@gmail.com>
2022-02-19 17:11:45 +0900

    fix readme

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -25,7 +25,6 @@ class SchedulesController < ApplicationController
 
   # POST /schedules    ajax
   def create
-    debugger
     schedule = Schedule.new(schedule_params)
     unless schedule.save
       flash[:create_schedule_error] = schedule.errors.full_messages
